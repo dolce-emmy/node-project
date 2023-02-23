e7eeb96b72c48b389ffe0cfd78549bc313e4538a
Eman Yassin <eman.yassin83@gmail.com>
2023-02-23 09:59:03 +0100

    node project

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv"
+import axios from "axios"
+
+
+//load our enviroment variables into process.env object
+dotenv.config()
+
+
+console.log(process.env)
+
+const app = express()
+
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/", (req,res) => {
+    res.send(process.env.SECRET_KEY)
+})
+
+
+app.get("/weather", async (req,res) => {
+    //proxy server means you are acting on someone's behalf
+
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.SECRET_KEY}`)
+
+    res.send(response.data)
+})
+
+app.listen(PORT, () => console.log("server is running on port:", PORT))
